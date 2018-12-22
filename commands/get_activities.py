465473a160cbe76d1d34e4f65216de16de114675
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-12-22 23:58:22 +0100

    Add more column to get_activities command

--- commands/get_activities.py
+++ commands/get_activities.py
@@ -6,7 +6,7 @@ from decorators import login_required, format_result, output_option
 
 @click.command()
 @output_option()
-@format_result(headers=['start_date', 'distance'])
+@format_result(headers=['start_date', 'distance', 'average_speed', 'max_speed', 'average_heartrate', 'max_heartrate'])
 @login_required
 def get_activities():
     return activities.get()
