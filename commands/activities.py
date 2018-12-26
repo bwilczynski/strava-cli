5c82daffb3edb3a0753654288a4e6022d8179583
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-12-26 20:47:40 +0100

    Show activity data in rows

--- commands/activities.py
+++ commands/activities.py
@@ -4,7 +4,17 @@ import api.activity
 from api import athlete
 from decorators import login_required, format_result, output_option
 from formatters import format_activity_type, format_date, format_distance, format_seconds, format_speed, \
-    format_heartrate
+    format_heartrate, format_elevation
+
+SUMMARY_ACTIVITY_FORMATTERS = {
+    'start_date': format_date,
+    'distance': format_distance,
+    'elapsed_time': format_seconds,
+    'average_speed': format_speed,
+    'max_speed': format_speed,
+    'average_heartrate': format_heartrate,
+    'max_heartrate': format_heartrate
+}
 
 SUMMARY_ACTIVITY_COLUMNS = ['start_date', 'name', 'distance', 'elapsed_time', 'average_speed', 'max_speed',
                             'average_heartrate',
@@ -19,14 +29,14 @@ SUMMARY_ACTIVITY_COLUMNS = ['start_date', 'name', 'distance', 'elapsed_time', 'a
 @format_result(table_columns=SUMMARY_ACTIVITY_COLUMNS)
 def get_activities(output, page, per_page):
     result = athlete.get_activities(page, per_page)
-    return result if output == 'json' else [_format_activity(activity) for activity in result]
+    return result if output == 'json' else [_format_summary_activity(activity) for activity in result]
 
 
 @click.command('activity')
 @click.option('--index', '-I', default=1, type=int)
 @output_option()
 @login_required
-@format_result(table_columns=SUMMARY_ACTIVITY_COLUMNS, single=True)
+@format_result(table_columns=['key', 'value'])
 def get_activity(index, output):
     activities = athlete.get_activities(index, 1)
     activity = activities[-1]
@@ -35,21 +45,48 @@ def get_activity(index, output):
     return result if output == 'json' else _format_activity(result)
 
 
-def _format_activity(activity):
+def _format_activity_name(name, activity):
+    activity_type = format_activity_type(activity.get('type'))
+    is_race = activity.get('workout_type', 0) == 1
+    return f'{activity_type} {click.style(name, bold=is_race)}'
+
+
+def _format_summary_activity(activity):
     def format_name(name):
-        activity_type = format_activity_type(activity['type'])
-        is_race = 'workout_type' in activity and activity['workout_type'] == 1
-        return f'{activity_type} {click.style(name, bold=is_race)}'
+        return _format_activity_name(name, activity)
 
     formatters = {
         'name': format_name,
-        'start_date': format_date,
-        'distance': format_distance,
-        'elapsed_time': format_seconds,
-        'average_speed': format_speed,
-        'max_speed': format_speed,
-        'average_heartrate': format_heartrate,
-        'max_heartrate': format_heartrate
+        **SUMMARY_ACTIVITY_FORMATTERS
     }
 
+    return _apply_formatters(activity, formatters)
+
+
+def _format_activity(activity):
+    def format_name(name):
+        return _format_activity_name(name, activity)
+
+    def no_formatter(value):
+        return value
+
+    def format_gear(gear):
+        return f'{gear.get("name")} ({format_distance(gear.get("distance", 0))})'
+
+    formatters = {
+        'name': format_name,
+        **SUMMARY_ACTIVITY_FORMATTERS,
+        'total_elevation_gain': format_elevation,
+        'calories': no_formatter,
+        'device_name': no_formatter,
+        'gear': format_gear,
+    }
+    activity = _apply_formatters(activity, formatters)
+
+    return [
+        {'key': k, 'value': v} for k, v in activity.items()
+    ]
+
+
+def _apply_formatters(activity, formatters):
     return {k: formatter(activity[k]) if k in activity else None for k, formatter in formatters.items()}
