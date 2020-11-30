3b7e2d2b141ece600de52b06ed8e0acea59424cd
Robert Auch <rauch@totalnetsolutions.net>
2020-11-30 16:27:52 -0600

    single event upload and batch event upload completed

--- strava/api/upload.py
+++ strava/api/upload.py
@@ -1,6 +1,16 @@
 from ._helpers import client, url, json
 
 
-def post_upload(xargs):
-    response = client.post(url(f'/uploads'), data=xargs)
+def post_upload(xargs, filename):
+    response=None
+    #with open(filename, 'rb') as f:
+    files={"file": open(filename, 'rb')}
+    response = client.post(url=url(f'/uploads'), data=xargs, files=files)
+    try:
+        return json(response)
+    except:
+        return response
+
+def get_upload(upload_id):
+    response = client.get(url(f'/uploads/{upload_id}'))
     return json(response)
